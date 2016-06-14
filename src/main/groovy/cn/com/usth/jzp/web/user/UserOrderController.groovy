0158d125356ad5ea28d0c8266576da58bbca0cd4
姜志鹏 <304636490@qq.com>
2016-06-14 23:06:22 +0800

    fix wrong code

--- src/main/groovy/cn/com/usth/jzp/web/user/UserOrderController.groovy
+++ src/main/groovy/cn/com/usth/jzp/web/user/UserOrderController.groovy
@@ -22,18 +22,7 @@ class UserOrderController {
     Order deal(Integer productId,String token) {
         orderService.deal(productId,token)
     }
-    User validate(String token) {
-        userService.validate(token)
-    }
-
-    @RequestMapping(value = "/password", method = RequestMethod.PUT)
-    Object refreshPassWord(@RequestBody Map map) {
-        Integer id = Integer.parseInt(map["id"].toString())
-        String oldPassword = map['oldPassword']
-        String newPassword = map['newPassword']
-        userService.refreshPassWord(id, oldPassword, newPassword)
-        [success: true]
-    }
+   
 
 
 }
